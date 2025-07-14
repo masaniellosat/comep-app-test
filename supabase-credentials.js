125227d0f9521cf276218d893279e3bca3187abd
masaniellosat <gcomep@gmail.com>
2025-07-14 13:17:12 +0200

    Update supabase-credentials.js

--- supabase-credentials.js
+++ supabase-credentials.js
@@ -1,3 +1,4 @@
-// supabase-credentials.js
-export const supabaseUrl = "https://orrnlfoqwiwdlykajikp.supabase.co";
-export const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ycm5sZm9xd2l2ZGx5a2FqaWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3NzIwNzksImV4cCI6MjA2NzM0ODA3OX0.Xp8MB-hbBmGoYtTrq9W-J64ERpe-FWF6lM01BvmEvu8";
+const supabaseUrl = "https://xsmpddhjfcaekdzqwvsz.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzbXBkZGhqZmNhZWtkenF3dnN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0Nzg1MjgsImV4cCI6MjA2ODA1NDUyOH0.yDflCx21WMPnNDn2dO5dbFa7zFRTLoxDrP1L5Qwl3S4";
+
+const client = supabase.createClient(supabaseUrl, supabaseKey);
